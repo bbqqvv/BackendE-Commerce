956144bf0cd6143ee18275c23023b1e1f73965cc
BUI QUOC VAN <116750804+bbqqvv@users.noreply.github.com>
2024-12-01 13:42:39 +0700

    🏗️ Config UP

---
+++ src/test/java/org/bbqqvv/backendecommerce/BackendECommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.bbqqvv.backendecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendECommerceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
